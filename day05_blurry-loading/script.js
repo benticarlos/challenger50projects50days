c23b29d206854bb61ee165e0e87b387dc0ceff31
Carlos Benti <benticarlos@gmail.com>
2023-05-22 11:04:28 +0200

    Added Day 05 JS script: script.js

---
+++ day05_blurry-loading/script.js
@@ -0,0 +1,17 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+function blurring () {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerHTML = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0 , 100, 30, 0)}px)`
+}
+
